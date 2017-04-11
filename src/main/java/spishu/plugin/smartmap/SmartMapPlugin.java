baec74f744c6ea437ede362ddcc874ffdcac0c3a
Joshua <Joshua@DESKTOP-RDTHTEN>
2017-04-10 20:25:31 -0500

    Fixed stuff

--- src/main/java/spishu/plugin/smartmap/SmartMapPlugin.java
+++ src/main/java/spishu/plugin/smartmap/SmartMapPlugin.java
@@ -1,10 +1,17 @@
 package spishu.plugin.smartmap;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -13,7 +20,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -32,15 +41,25 @@ public class SmartMapPlugin extends JavaPlugin implements Listener {
     static ItemStack MAP_ITEM;
     static Gson GSON;
     
+    //Map<String,SmartMapApp> apps;
     Map<String,SmartMapApp> apps;
     List<Integer> maps;
+    static BufferedImage chip8; //FOR TESTING ONLY
+    static BufferedImage select;
+    static String currentApp = "";
     
-    static {
+    {
     	
     	MAP_RENDERER = new MapRenderer(){
     		public void render(MapView view, MapCanvas canvas, Player player) {
     			canvas.drawText(10, 0, MinecraftFont.Font, "Welcome to SmartMap OS!\n\n          V0.0.1 Beta!");
-    			
+    			canvas.drawText(20, 40, MinecraftFont.Font, "Applications:");
+    			for(String name : apps.keySet()) { //TODO: Implement the app listing
+    				canvas.drawImage(0,50,chip8);
+    				currentApp = name;
+    			}
+    			canvas.drawImage(0,50,chip8);
+    			canvas.drawImage(32, 82,select);
     		}
     	};
     	
@@ -73,11 +92,41 @@ public class SmartMapPlugin extends JavaPlugin implements Listener {
 			MapView map = Bukkit.getServer().createMap(getServer().getWorlds().get(0)); //Arbitrary world
 			clearRenderers(map);
 			mapItem.setDurability(map.getId());
+			map.addRenderer(MAP_RENDERER);
 		}
+	}
+	
+	@EventHandler 
+	public void playerInteract(PlayerInteractEvent e){
+	  //this will be called automatically by bukkit whenever a player interacts
+	  if(e.getAction().equals(Action.RIGHT_CLICK_BLOCK) || e.getAction().equals(Action.RIGHT_CLICK_AIR)){
+		  if(e.getPlayer().isSneaking()){
+		  //if(args.length < 1) sender.sendMessage("Invalid number of arguments.");
+			//else {
+				//if(!(sender instanceof Player)) sender.sendMessage("Only players may use this command.");
+				//else {
+					if(!apps.containsKey(currentApp)) e.getPlayer().sendMessage("The app specified does not exist.");
+					else {
+						Player player = e.getPlayer();
+						ItemStack mapItem = player.getInventory().getItemInMainHand();
+						if(mapItem.getType() != Material.MAP) e.getPlayer().sendMessage("You must be holding a map.");
+						else switchApp(mapItem, currentApp);
+					//}
+				//}
+					}
+			}
+	  }
 	}
 	
 	//TODO API for filesystem
 	public void onEnable() {
+		try {
+			chip8 = ImageIO.read(new File(this.getDataFolder().getPath() + "/Chip8.png")); //FOR TESTING ONLY
+			select = ImageIO.read(new File(this.getDataFolder().getPath() + "/select.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		getServer().getPluginManager().registerEvents(this, this);
 		getServer().addRecipe(MAP_RECIPE); //Set up the recipe after onEnable so we know the server is entirely loaded.
 		apps = new HashMap<String,SmartMapApp>(); //Initialize app map
@@ -132,6 +181,12 @@ public class SmartMapPlugin extends JavaPlugin implements Listener {
 			for(String name : apps.keySet()) {
 				sender.sendMessage(name);
 			}
+		} else if(cmd.getName().equalsIgnoreCase("returntohome")) {
+			ItemStack mapItem = ((Player) sender).getItemInHand();
+			MapView map = Bukkit.getServer().createMap(getServer().getWorlds().get(0)); //Arbitrary world
+			clearRenderers(map);
+			mapItem.setDurability(map.getId());
+			map.addRenderer(MAP_RENDERER);
 		}
 		return true; 
 	}
